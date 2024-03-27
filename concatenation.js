3f064dcabba2ef5288588c1d70a47c3a1ee7d062
valentinfcode <147728698+valentinfcode@users.noreply.github.com>
2024-03-27 13:16:18 +0000

    finsihed tasks and added an img when the submit button is clicked

--- concatenation.js
+++ concatenation.js
@@ -1,5 +1,6 @@
 let submitButton = document.querySelector(".submit");
 let results = document.querySelector(".results");
+let snoopy = document.querySelector(".snoopy");
 
 submitButton.addEventListener("click", function() {
     results.style.display = "block";
@@ -9,11 +10,17 @@ submitButton.addEventListener("click", function() {
     let birthYear = document.querySelector(".year").value;
     let age = 2023 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let city = document.querySelector(".city").value;
     // 2. Below write your sentences! 
+    // use results.innerhtml
+
+    results.innerHTML = "My name is " + myName + ". I am feeling " + mood + " today. My favorite food is " + food + ". I was born one the year of " + birthYear + ". I live in " + city + ".";
+
+    let snoopy = document.querySelector(".snoopy");
+    snoopy.style.display = "block";
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
+    
 
 });
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let buttonOne = document.querySelector(".button-one");
 let captionOne = document.querySelector(".caption-one");
+let buttontwo = document.querySelector(".button-two");
+let captionTwo = document.querySelector(".caption-two");
 
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
-
-
+    let spidyCaption = document.querySelector(".comment-one").value;
+    console.log(spidyCaption); 
+    captionOne.innerHTML = spidyCaption;
     // 2. Display the variable spidyCaption to captionOne.
-
+    
 
 });
 
 // 3. Repeat the previous steps for the second meme.
 //  - Be sure to store variables for the button and caption.
+
+buttontwo.addEventListener('click', function(){
+  let kermitCaption = document.querySelector(".comment-two").value;
+  console.log(kermitCaption); 
+  captionTwo.innerHTML = kermitCaption;
+});
